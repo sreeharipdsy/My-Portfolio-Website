2130b845cbe89690e3a45882d42ae3e48f5de579
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-07-11 18:19:09 +0530

    update

---
+++ portfolio/assets/js/open-link.js
@@ -0,0 +1,29 @@
+function openResume() {
+    window.open('assets/pdf/Resume_Sreehari_S.pdf'); 
+    return true;    
+}
+
+function projectOrigami() {
+    window.location.href="https://sreeharipdsy.github.io/origami";
+    return true;
+}
+
+function projectSecuCenter() {
+    window.location.href="https://sreeharipdsy.github.io/secucenter-project";
+    return true;
+}
+
+function projectDreamCake() {
+    window.location.href="https://sreeharipdsy.github.io/Dream-Bake-Project";
+    return true;
+}
+
+function projectWyteBirds() {
+    window.location.href="https://sreeharipdsy.in/wytebirds";
+    return true;
+}
+
+function projectPersonal() {
+    window.location.href="https://sreeharipdsy.in";
+    return true;
+}
--- portfolio/assets/js/open-resume.js
+++
@@ -1,4 +0,0 @@
-function openResume() {
-    window.open('assets/pdf/Resume_Sreehari_S.pdf'); 
-    return true;    
-}
